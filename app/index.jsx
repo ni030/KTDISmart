4d4345f78433730e47e45faaf864abda45325741
JIAAN <an02@gradute.utm.my>
2024-10-16 15:39:00 +0800

    ababa

--- app/index.jsx
+++ app/index.jsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Welcome to KTDI the Smart</Text>
+      <Text>Welcome to KTDI the Smart ababababa</Text>
     </View>
   );
 }
