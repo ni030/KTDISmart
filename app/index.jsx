137dc476e93766662d77c34bd305d77c96324b2b
feizhen18 <f.zhen2003@gmail.com>
2024-10-16 14:11:42 +0800

    1st change

--- app/index.jsx
+++ app/index.jsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Welcome to KTDI Smart</Text>
+      <Text>Welcome to KTDI de Smart</Text>
     </View>
   );
 }
