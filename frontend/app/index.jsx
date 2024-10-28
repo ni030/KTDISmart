252f6a6ac56fc8c42d3a50e05edda2f6cb931d70
LIM SI NI <limsini000@gmail.com>
2024-10-28 14:46:02 +0800

    temporary home page

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Link } from 'expo-router';
 import { Redirect } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Pressable, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
-  return <Redirect href="/(auth)/sign-up" />;
-};
+  // return <Redirect href="/(auth)/sign-up" />;
+  // temporary before navbar is done
+  return (
+    <SafeAreaView className="w-full h-screen flex flex-1 justify-start items-center bg-blue-100">
+      <View className="w-full h-1/3 flex justify-center items-center">
+        <Text className="text-center text-3xl text-red-900 font-semibold">Welcome to KTDI SMART</Text>
+      </View>
+      <View className="w-4/5 flex justify-center items-center gap-3">
+          <TouchableOpacity className="bg-red-500 py-4 px-6 rounded-lg mb-4">
+            <Link href="/(auth)/sign-in">
+              <Text className="text-xl text-white font-bold text-center">Sign In</Text>
+            </Link>
+          </TouchableOpacity>
+       
+          <TouchableOpacity className="bg-red-500 py-4 px-6 rounded-lg mb-4">
+            <Link href="/(ktdi-merit)">
+              <Text className="text-xl text-white font-bold text-center">KTDI Merit</Text>
+            </Link>
+          </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
