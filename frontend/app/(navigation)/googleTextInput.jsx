e2f8d4aa19e446d53e0ea002c0e74525e1b15d6d
JIAAN <an02@gradute.utm.my>
2024-11-01 21:51:53 +0800

    add googleTextInput file

---
+++ frontend/app/(navigation)/googleTextInput.jsx
@@ -0,0 +1,11 @@
+import {Text,View} from "react-native";
+
+const googleTextInput=()=>{
+    <View>
+        <Text>
+            Search
+        </Text>
+    </View>
+};
+
+export default googleTextInput;
