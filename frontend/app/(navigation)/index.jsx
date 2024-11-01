2a0d4c288534d3d560079a3ef5b5450e79713a58
JIAAN <an02@gradute.utm.my>
2024-11-01 21:50:34 +0800

    add googleTextInput

--- frontend/app/(navigation)/index.jsx
+++ frontend/app/(navigation)/index.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
+import googleTextInput from './googleTextInput';
 
 export default function index() {
+  const handleDestinationPress=()=>{
+
+  };
+
   return (
     <SafeAreaView>
       <Text>Welcome to Navigation Page</Text>
+      <googleTextInput
+        containerStyle="bg-white shadow-md shadow-neutral-300"
+        handlePress={handleDestinationPress}
+      />
+      <Text className="text-xl font-JakartaBold mt-5 mb-3">
+        Your Current Location
+      </Text>
+      <View className="flex flex-row items-center bg-transparent h-[300px]">
+        <Map />
+      </View>
+      <Text className="text-xl font-JakartaBold mt-5 mb-3">
+        Recent Searches
+      </Text>
     </SafeAreaView>
   );
 }
