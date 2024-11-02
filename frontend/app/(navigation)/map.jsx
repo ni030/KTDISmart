99b64600723b90c8094158cefd72cd523117a592
JIAAN <an02@graduate.utm.my>
2024-11-02 21:24:38 +0800

    add Google API

--- frontend/app/(navigation)/map.jsx
+++ frontend/app/(navigation)/map.jsx
@@ -1,9 +1,12 @@
 import {Text,View} from "react-native";
+import MapView, {PROVIDER_DEFAULT} from "react-native-maps";
 const Map=()=>{
     return(
-        <View>
+        <MapView
+            provider={PROVIDER_DEFAULT} className="w-full h-full rounded-2xl"
+        >
             <Text>Map</Text>
-        </View>
+        </MapView>
     );
 };
 
