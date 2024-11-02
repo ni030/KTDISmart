5d877776cd9c60cd255c9446dcdc605cfa95788f
LIM SI NI <limsini000@gmail.com>
2024-11-02 13:52:00 +0800

    root color and font

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -6,6 +6,7 @@ import { Pressable, Text, TouchableOpacity, View } from 'react-native';
 import { useFonts } from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import * as SplashScreen from 'expo-splash-screen';
+import { PaperProvider } from 'react-native-paper';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -32,26 +33,30 @@ export default function App() {
     return null; // Return null so nothing renders until fonts are ready
   }
 
-  // return <Redirect href="/(ktdi-merit)" />;
+  return(
+    <PaperProvider>
+     <Text> <Redirect href="/(ktdi-merit)" />;</Text>
+    </PaperProvider>
+  )
   // temporary before navbar is done
-  return (
-    <SafeAreaView className="w-full h-screen flex flex-1 justify-start items-center bg-blue-100">
-      <View className="w-full h-1/3 flex justify-center items-center">
-        <Text className="text-center text-3xl text-red-900 font-semibold">Welcome to KTDI SMART</Text>
-      </View>
-      <View className="w-4/5 flex justify-center items-center gap-3">
-          <TouchableOpacity className="bg-red-500 py-4 px-6 rounded-lg mb-4">
-            <Link href="/(auth)/sign-in">
-              <Text className="text-xl text-white font-bold text-center">Sign In</Text>
-            </Link>
-          </TouchableOpacity>
+  // return (
+  //   <SafeAreaView className="w-full h-screen flex flex-1 justify-start items-center bg-blue-100">
+  //     <View className="w-full h-1/3 flex justify-center items-center">
+  //       <Text className="text-center text-3xl text-red-900 font-semibold">Welcome to KTDI SMART</Text>
+  //     </View>
+  //     <View className="w-4/5 flex justify-center items-center gap-3">
+  //         <TouchableOpacity className="bg-red-500 py-4 px-6 rounded-lg mb-4">
+  //           <Link href="/(auth)/sign-in">
+  //             <Text className="text-xl text-white font-bold text-center">Sign In</Text>
+  //           </Link>
+  //         </TouchableOpacity>
        
-          <TouchableOpacity className="bg-red-500 py-4 px-6 rounded-lg mb-4">
-            <Link href="/(ktdi-merit)">
-              <Text className="text-xl text-white font-bold text-center">KTDI Merit</Text>
-            </Link>
-          </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
+  //         <TouchableOpacity className="bg-red-500 py-4 px-6 rounded-lg mb-4">
+  //           <Link href="/(ktdi-merit)">
+  //             <Text className="text-xl text-white font-bold text-center">KTDI Merit</Text>
+  //           </Link>
+  //         </TouchableOpacity>
+  //     </View>
+  //   </SafeAreaView>
+  // );
 }
