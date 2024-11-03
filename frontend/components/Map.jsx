f0d70fd4577a6d539b82b254afc16124b9a55d62
JIAAN <an02@graduate.utm.my>
2024-11-03 23:28:14 +0800

    change map display

--- frontend/components/Map.jsx
+++ frontend/components/Map.jsx
@@ -16,8 +16,8 @@ const Map=()=>{
         <MapView
         provider={PROVIDER_DEFAULT}
         className="w-full h-full rounded-2xl"
-        tintColor="black"
-        mapType="mutedStandard"
+        // tintColor="black"
+        // mapType="mutedStandard"
         showsPointsOfInterest={false}
         initialRegion={region}
         showsUserLocation={true}
