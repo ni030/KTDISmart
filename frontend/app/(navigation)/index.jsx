1042582b4cfde2b85a0305baa0fd852bab7dde99
JIAAN <an02@graduate.utm.my>
2024-11-06 13:52:30 +0800

    change navigation index

--- frontend/app/(navigation)/index.jsx
+++ frontend/app/(navigation)/index.jsx
@@ -2,7 +2,7 @@ import * as Location from 'expo-location'
 import React from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
 import GoogleTextInput from '../../components/navigation/GoogleTextInput';
-import Map from '../../components/Map';
+import Map from '../../components/navigation/Map';
 import { useLocationStore } from '../../store';
 import { useEffect, useState } from 'react';
 
