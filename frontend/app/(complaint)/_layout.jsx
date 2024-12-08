303407bbd47e07126a57972f3da999478de4f102
feizhen18 <f.zhen2003@gmail.com>
2024-12-08 22:35:01 +0800

    header false

--- frontend/app/(complaint)/_layout.jsx
+++ frontend/app/(complaint)/_layout.jsx
@@ -5,10 +5,10 @@ export default function Layout() {
   return (
     <>
     <Stack>
-      <Stack.Screen name="index" options={{title: "Complaint History"}}/>
-      <Stack.Screen name="category" options={{title: "Complaint Subsystem"}}/>
-      <Stack.Screen name="report" options={{title: "Report form"}}/>
-      <Stack.Screen name="progress" options={{title: "Tracking Complaint Progress"}}/>
+      <Stack.Screen name="index" options={{headerShown: false}}/>
+      <Stack.Screen name="category" options={{headerShown: false}}/>
+      <Stack.Screen name="report" options={{headerShown: false}}/>
+      <Stack.Screen name="progress" options={{headerShown: false}}/>
     </Stack>
 
     <BottomNavigationBar />
