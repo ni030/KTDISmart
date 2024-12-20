186204016fffcf72ebd68cc0f783f6ed1fbbd3f4
JIAAN <an02@graduate.utm.my>
2024-12-20 12:05:42 +0800

    complete navigation sprint 3

--- backend/routes/navigationRoute.js
+++ backend/routes/navigationRoute.js
@@ -4,6 +4,5 @@ const navigationController = require("../controllers/navigationController")
 
 router.post('/create/location', navigationController.saveCurrentLocation)
 router.post('/create/search', navigationController.saveSearchLocation)
-router.get('/check/:matricNum',navigationController.fetchSearchLocation)
 
 module.exports = router
